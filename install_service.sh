f2257d51c29c70ed91885db9598263806a28f283
Obszczymucha <obszczymucha@gmail.com>
2024-09-18 21:12:16 +1000

    Add install_service.sh.

---
+++ install_service.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+function main() {
+  sudo cp service/* /etc/systemd/system/
+  sudo systemctl daemon-reload
+  sudo systemctl enable barrier.timer
+  sudo systemctl enable barrier_stop.timer
+  sudo systemctl restart barrier.timer
+  sudo systemctl restart barriet_stop.timer
+}
+
+main "$@"
+
--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 function main() {
+  sudo systemctl stop barrier.service
   ./build.sh && sudo build/bin/barrierc -f --name pi 192.168.1.198:9696
 }
 
