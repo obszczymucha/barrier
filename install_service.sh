51b79e18aad6037d3a4263079c19c44795ea91b0
Obszczymucha <obszczymucha@gmail.com>
2024-09-20 19:18:03 +1000

    Pretty print service status after installing.

--- install_service.sh
+++ install_service.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2155 # Declare and assign separately.
+
+function systemctl_property() {
+  systemctl show --property="$1" barrier.service | sed 's/.*=//'
+}
 
 function main() {
   sudo cp service/* /etc/systemd/system/
@@ -7,6 +12,16 @@ function main() {
   sudo systemctl enable barrier_stop.timer
   sudo systemctl restart barrier.timer
   sudo systemctl restart barrier_stop.timer
+
+  echo "barrier.service status:" >&2
+
+  local loaded=$(systemctl_property "LoadState")
+  local active=$(systemctl_property "ActiveState")
+  local triggered_by=$(systemctl_property "TriggeredBy")
+  local next_trigger=$(systemctl_property "NextElapseUSecRealtime")
+
+  echo "Status: $active ($loaded)"
+  echo "Next trigger: $next_trigger ($triggered_by)"
 }
 
 main "$@"
