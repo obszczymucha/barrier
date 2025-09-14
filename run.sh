41c22b1f44b8b5f3894a6153b78efa299889d53a
Obszczymucha <obszczymucha@gmail.com>
2025-09-15 09:30:02 +1000

    Remap for linux server (fix properly later if needed).

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 function main() {
   sudo systemctl stop barrier.service
-  ./build.sh && sudo build/bin/barrierc -f --name pi 192.168.1.198:9696
+  ./build.sh && sudo build/bin/barrierc -f --name pi 192.168.1.196:9696
 }
 
 main "$@"
--- src/lib/platform/UsbKeyState.cpp
+++ src/lib/platform/UsbKeyState.cpp
@@ -44,6 +44,7 @@ UInt8 map_key(KeyID id) {
 }
 
 void UsbKeyState::send_key(unsigned char modifier, unsigned char key) {
+  // LOG((CLOG_INFO "send_key called: modifier=%d, key=%d, fd=%d", modifier, key, m_fd));
   unsigned char report[8] = {modifier, 0, key, 0, 0, 0, 0, 0};
   int result = write(m_fd, report, sizeof(report));
   if (result < 0) {
@@ -52,6 +53,7 @@ void UsbKeyState::send_key(unsigned char modifier, unsigned char key) {
 }
 
 unsigned char map_mask(KeyModifierMask mask) {
+  // LOG((CLOG_INFO "map_mask called: mask=%d", mask));
   switch (mask) {
   case 16:
     return 0x08; // LEFT_SUPER
@@ -75,264 +77,265 @@ void UsbKeyState::send_barrier_key(KeyID id, KeyModifierMask mask) {
 void UsbKeyState::fakeKeyDown(KeyID id, KeyModifierMask mask,
                               KeyButton button) {
   UInt8 key = 0;
+  // LOG((CLOG_INFO "UsbKeyState::fakeKeyDown(%d, %d, %d) modifier: %d", id, mask, button, m_modifier));
 
   switch (button) {
   case 0:
     send_barrier_key(id, mask);
     return;
-  case 29: // LEFT_CTRL
+  case 37: // LEFT_CTRL
     m_modifier |= 0x01;
     break;
-  case 42: // LEFT_SHIFT
+  case 50: // LEFT_SHIFT
     m_modifier |= 0x02;
     break;
-  case 56: // LEFT_ALT
+  case 64: // LEFT_ALT
     m_modifier |= 0x04;
     break;
-  case 347: // LEFT_SUPER
+  case 133: // LEFT_SUPER
     m_modifier |= 0x08;
     break;
-  case 285: // RIGHT_CTRL
+  case 293: // RIGHT_CTRL
     m_modifier |= 0x10;
     break;
-  case 54: // RIGHT_SHIFT
+  case 62: // RIGHT_SHIFT
     m_modifier |= 0x20;
     break;
-  case 312: // RIGHT_ALT
+  case 320: // RIGHT_ALT
     m_modifier |= 0x40;
     break;
-  case 348: // RIGHT_SUPER
+  case 356: // RIGHT_SUPER
     m_modifier |= 0x80;
     break;
-  case 1: // ESC
+  case 9: // ESC
     key = 0x29;
     break;
-  case 2: // 1
+  case 10: // 1
     key = 0x1e;
     break;
-  case 3: // 2
+  case 11: // 2
     key = 0x1f;
     break;
-  case 4: // 3
+  case 12: // 3
     key = 0x20;
     break;
-  case 5: // 4
+  case 13: // 4
     key = 0x21;
     break;
-  case 6: // 5
+  case 14: // 5
     key = 0x22;
     break;
-  case 7: // 6
+  case 15: // 6
     key = 0x23;
     break;
-  case 8: // 7
+  case 16: // 7
     key = 0x24;
     break;
-  case 9: // 8
+  case 17: // 8
     key = 0x25;
     break;
-  case 10: // 9
+  case 18: // 9
     key = 0x26;
     break;
-  case 11: // 0
+  case 19: // 0
     key = 0x27;
     break;
-  case 12: // -
+  case 20: // -
     key = 0x2D;
     break;
-  case 13: // =
+  case 21: // =
     key = 0x2E;
     break;
-  case 14: // Backspace
+  case 22: // Backspace
     key = 0x2A;
     break;
-  case 15: // Tab
+  case 23: // Tab
     key = 0x2B;
     break;
-  case 16: // Q
+  case 24: // Q
     key = 0x14;
     break;
-  case 17: // W
+  case 25: // W
     key = 0x1A;
     break;
-  case 18: // E
+  case 26: // E
     key = 0x08;
     break;
-  case 19: // R
+  case 27: // R
     key = 0x15;
     break;
-  case 20: // T
+  case 28: // T
     key = 0x17;
     break;
-  case 21: // Y
+  case 29: // Y
     key = 0x1C;
     break;
-  case 22: // U
+  case 30: // U
     key = 0x18;
     break;
-  case 23: // I
+  case 31: // I
     key = 0x0C;
     break;
-  case 24: // O
+  case 32: // O
     key = 0x12;
     break;
-  case 25: // P
+  case 33: // P
     key = 0x13;
     break;
-  case 26: // [
+  case 34: // [
     key = 0x2F;
     break;
-  case 27: // ]
+  case 35: // ]
     key = 0x30;
     break;
-  case 28: // Enter
+  case 36: // Enter
     key = 0x28;
     break;
-  case 30: // A
+  case 38: // A
     key = 0x04;
     break;
-  case 31: // S
+  case 39: // S
     key = 0x16;
     break;
-  case 32: // D
+  case 40: // D
     key = 0x07;
     break;
-  case 33: // F
+  case 41: // F
     key = 0x09;
     break;
-  case 34: // G
+  case 42: // G
     key = 0x0A;
     break;
-  case 35: // H
+  case 43: // H
     key = 0x0B;
     break;
-  case 36: // J
+  case 44: // J
     key = 0x0D;
     break;
-  case 37: // K
+  case 45: // K
     key = 0x0E;
     break;
-  case 38: // L
+  case 46: // L
     key = 0x0F;
     break;
-  case 39: // ;
+  case 47: // ;
     key = 0x33;
     break;
-  case 40: // '
+  case 48: // '
     key = 0x34;
     break;
-  case 41: // `
+  case 49: // `
     key = 0x35;
     break;
-  case 43: // Backslash
+  case 51: // Backslash
     key = 0x31;
     break;
-  case 44: // Z
+  case 52: // Z
     key = 0x1D;
     break;
-  case 45: // X
+  case 53: // X
     key = 0x1B;
     break;
-  case 46: // C
+  case 54: // C
     key = 0x06;
     break;
-  case 47: // V
+  case 55: // V
     key = 0x19;
     break;
-  case 48: // B
+  case 56: // B
     key = 0x05;
     break;
-  case 49: // N
+  case 57: // N
     key = 0x11;
     break;
-  case 50: // M
+  case 58: // M
     key = 0x10;
     break;
-  case 51: // ,
+  case 59: // ,
     key = 0x36;
     break;
-  case 52: // .
+  case 60: // .
     key = 0x37;
     break;
-  case 53: // /
+  case 61: // /
     key = 0x38;
     break;
-  case 57: // Spacebar
+  case 65: // Spacebar
     key = 0x2C;
     break;
-  case 58: // CapsLock
+  case 66: // CapsLock
     key = 0x39;
     break;
-  case 59: // F1
+  case 67: // F1
     key = 0x3A;
     break;
-  case 60: // F2
+  case 68: // F2
     key = 0x3B;
     break;
-  case 61: // F3
+  case 69: // F3
     key = 0x3C;
     break;
-  case 62: // F4
+  case 70: // F4
     key = 0x3D;
     break;
-  case 63: // F5
+  case 71: // F5
     key = 0x3E;
     break;
-  case 64: // F6
+  case 72: // F6
     key = 0x3F;
     break;
-  case 65: // F7
+  case 73: // F7
     key = 0x40;
     break;
-  case 66: // F8
+  case 74: // F8
     key = 0x41;
     break;
-  case 67: // F9
+  case 75: // F9
     key = 0x42;
     break;
-  case 68: // F10
+  case 76: // F10
     key = 0x43;
     break;
-  case 69: // Pause
+  case 77: // Pause
     key = 0x48;
     break;
-  case 87: // F11
+  case 95: // F11
     key = 0x44;
     break;
-  case 88: // F12
+  case 96: // F12
     key = 0x45;
     break;
-  case 311: // PrntScr
+  case 107: // PrntScr
     key = 0x46;
     break;
-  case 338: // Insert
+  case 118: // Insert
     key = 0x49;
     break;
-  case 339: // Delete
+  case 119: // Delete
     key = 0x4C;
     break;
-  case 327: // Home
+  case 110: // Home
     key = 0x4A;
     break;
-  case 335: // End
+  case 115: // End
     key = 0x4D;
     break;
-  case 329: // PageUp
+  case 112: // PageUp
     key = 0x4B;
     break;
-  case 337: // PageDown
+  case 117: // PageDown
     key = 0x4E;
     break;
-  case 331: // Left
+  case 113: // Left
     key = 0x50;
     break;
-  case 333: // Right
+  case 114: // Right
     key = 0x4F;
     break;
-  case 328: // Up
+  case 111: // Up
     key = 0x52;
     break;
-  case 336: // Down
+  case 116: // Down
     key = 0x51;
     break;
   }
@@ -358,9 +361,7 @@ void UsbKeyState::fakeKeyDown(KeyID id, KeyModifierMask mask,
     }
   }
 
-  /*LOG((CLOG_INFO "UsbKeyState::fakeKeyDown(%d, %d, %d)", id, mask, button));*/
-  /*LOG((CLOG_INFO "modifier: %d", m_modifier));*/
-
+  // LOG((CLOG_INFO "About to send_key: modifier=%d, key=%d, button=%d", m_modifier, key, button));
   send_key(m_modifier, key);
 }
 
@@ -378,31 +379,31 @@ bool UsbKeyState::fakeKeyRepeat(KeyID id, KeyModifierMask mask, SInt32 count,
 }
 
 bool UsbKeyState::fakeKeyUp(KeyButton button) {
-  /*LOG((CLOG_INFO "UsbKeyState::fakeKeyUp(%d)", button));*/
+  // LOG((CLOG_INFO "UsbKeyState::fakeKeyUp(%d)", button));
 
   switch (button) {
-  case 29: // LEFT_CTRL
+  case 37: // LEFT_CTRL
     m_modifier &= ~0x01;
     break;
-  case 42: // LEFT_SHIFT
+  case 50: // LEFT_SHIFT
     m_modifier &= ~0x02;
     break;
-  case 56: // LEFT_ALT
+  case 64: // LEFT_ALT
     m_modifier &= ~0x04;
     break;
-  case 347: // LEFT_SUPER
+  case 133: // LEFT_SUPER
     m_modifier &= ~0x08;
     break;
-  case 285: // RIGHT_CTRL
+  case 293: // RIGHT_CTRL
     m_modifier &= ~0x10;
     break;
-  case 54: // RIGHT_SHIFT
+  case 62: // RIGHT_SHIFT
     m_modifier &= ~0x20;
     break;
-  case 312: // RIGHT_ALT
+  case 320: // RIGHT_ALT
     m_modifier &= ~0x40;
     break;
-  case 348: // RIGHT_SUPER
+  case 356: // RIGHT_SUPER
     m_modifier &= ~0x80;
     break;
   }
